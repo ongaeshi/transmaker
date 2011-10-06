3a1710ded2608b469712e7452e9f31bda53d5fd3
ongaeshi <ongaeshi0621@gmail.com>
2011-10-07 03:21:35 +0900

    ページ内を翻訳テキストで書き換えられるように(まだ警告がいっぱい出る)

--- data/selection-text.js
+++ data/selection-text.js
@@ -3,8 +3,23 @@
 // @author ongaeshi
 // @date   2011/10/04
 
-self.on("click", function () {
-  var text = window.getSelection().toString();
-  self.postMessage(text);
+var translateNode;
+
+self.port.on("replace-select", function() {
+  // @todo 複数ウィンドウ対応
+  // @todo 複数範囲に対応
+  if (window.getSelection()) {
+    var range = window.getSelection().getRangeAt(0);
+
+    if (range.startContainer.nodeType == 3) {
+      translateNode = range.startContainer;
+      self.port.emit("translate", translateNode.wholeText);
+    }
+  }
+});
+
+self.port.on("replace", function (dst) {
+  // @todo アニメーション処理
+  translateNode.replaceWholeText(dst);
 });
 
