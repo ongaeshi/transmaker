e7b7d02542f2efd2cb0383192d4be129c4b1fdbd
ongaeshi <ongaeshi0621@gmail.com>
2011-10-08 23:11:37 +0900

    バグ修正、detachされたworkerの削除、選択範囲の無いWindowでも正しく動作するように。

--- data/selection-text.js
+++ data/selection-text.js
@@ -6,14 +6,20 @@
 var translateNode;
 
 self.port.on("replace-select", function() {
+  var selection = window.getSelection();
+
   // @todo 複数ウィンドウ対応
   // @todo 複数範囲に対応
-  if (window.getSelection()) {
-    var range = window.getSelection().getRangeAt(0);
-
-    if (range.startContainer.nodeType == 3) {
-      translateNode = range.startContainer;
-      self.port.emit("translate", translateNode.wholeText);
+  if (selection && selection.rangeCount) {
+    var range = selection.getRangeAt(0);
+      
+    if (range) {
+      if (range.startContainer.nodeType == 3) {
+        // @todo 一時変数に保存するのでは無く、コンテナに貯蓄してidを渡すのが良さそう
+        // @todo rangeを一気に解析してコンテナに保持、検索先でいい感じにする
+        translateNode = range.startContainer; 
+        self.port.emit("translate", translateNode.wholeText);
+      }
     }
   }
 });
