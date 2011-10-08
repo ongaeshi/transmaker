76d42d11a72ef9f7c7c822764caa8088e2f99a8e
ongaeshi <ongaeshi0621@gmail.com>
2011-10-09 03:11:47 +0900

    複数の選択範囲に対応

--- data/selection-text.js
+++ data/selection-text.js
@@ -12,13 +12,16 @@ var gSelectNodes;
 self.port.on("replace-select", function() {
   var selection = window.getSelection();
 
-  if (selection && selection.rangeCount) {
-    var range = selection.getRangeAt(0); // @todo 複数の選択範囲に対応
+  if (selection) {
+    var selectNodes = [];
+    
+    for (var i = 0; i < selection.rangeCount; i++) {
+      var range = selection.getRangeAt(i);
       
-    if (range) {
-      var selectNodes = Range_walk(
+      var result = Range_walk(
         range,
         function (node) {
+          // @todo startOffset, endOffsetに対応
           if (node.nodeType == 3) {
             var text = node.wholeText;
             if (text.replace(/[ \t\n]/g, "").length > 0)
@@ -27,16 +30,18 @@ self.port.on("replace-select", function() {
         },
         []);
 
-      var srcArray = selectNodes.map(function(v) {
-        return v.text;
-      });
+      selectNodes = selectNodes.concat(result);
+    }
 
-      if (srcArray.length > 0) {
-        // @todo 一時変数に保存するのでは無く、コンテナに貯蓄してidを渡すのが良さそう
-        // @todo rangeを一気に解析してコンテナに保持、検索先でいい感じにする
-        gSelectNodes = selectNodes;
-        self.port.emit("translate", srcArray);
-      }
+    var srcArray = selectNodes.map(function(v) {
+      return v.text;
+    });
+
+    if (srcArray.length > 0) {
+      // @todo 一時変数に保存するのでは無く、コンテナに貯蓄してidを渡すのが良さそう
+      // @todo rangeを一気に解析してコンテナに保持、検索先でいい感じにする
+      gSelectNodes = selectNodes;
+      self.port.emit("translate", srcArray);
     }
   }
 });
