18fe7a709ed3abb1d6b09b5260480915a51318e5
ongaeshi <ongaeshi0621@gmail.com>
2011-10-10 23:06:44 +0900

    undoにアニメーションを付ける

--- data/text-anim.js
+++ data/text-anim.js
@@ -11,6 +11,12 @@ var easeOutQuad = function (t, b, c, d) {
   return -c *(t/=d)*(t-2) + b;
 }
 
+var linear = function(t, b, c, d) {
+  //return firstNum + diff * p;
+  return c * (t/=d) + b;
+}
+
+
 var replaceAnimation = function (node, dst) {
   // パラメータ
   const CHANGE_MSEC   = 300,
@@ -62,5 +68,30 @@ var insertAnimation = function (node, insertText) {
 
 var undoAnimation = function (node, dst) {
   // @todo アニメーションにすると上手くいかない？要検証。
-  node.replaceWholeText(dst);
+  //node.replaceWholeText(dst);
+  //replaceAnimation(node, dst);
+
+  // パラメータ
+  const CHANGE_MSEC   = 200,
+        INTERVAL_MSEC = 32; // 30F
+
+  // 作業用変数
+  var src = node.wholeText;
+  var currentTime = 0;
+
+  // アニメーション処理
+  var timer = setInterval(function() {
+    var rate = 1.0 - linear(currentTime, 0, 1, CHANGE_MSEC);
+
+    node.replaceWholeText(src.substring(0, src.length * rate) +
+                          dst.substring(dst.length * rate, dst.length));
+
+    currentTime += INTERVAL_MSEC;
+
+    if (currentTime >= CHANGE_MSEC) {
+      node.replaceWholeText(dst);
+      clearInterval(timer);
+    }
+  }, INTERVAL_MSEC);
+  
 }
