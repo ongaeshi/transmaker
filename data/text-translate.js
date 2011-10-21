689a75576559d0a3a1906199a8564ac9323be9dc
ongaeshi <ongaeshi0621@gmail.com>
2011-10-22 02:59:21 +0900

    他言語→母国語に翻訳出来るように。

--- data/text-translate.js
+++ data/text-translate.js
@@ -1,5 +1,11 @@
+// 翻訳結果を送信
 $(function() {
   $("#translate-button").click(function(){
     self.postMessage($("#src > textarea").val());
   });
+});
+
+// 翻訳結果を受け取る
+self.on('message', function(msg) {
+  $("#dst").text(msg);
 });
