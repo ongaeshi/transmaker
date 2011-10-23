e73ab7c8e2a29c902e443bc963d92593b8645e0b
ongaeshi <ongaeshi0621@gmail.com>
2011-10-23 15:31:35 +0900

    閉じるボタンの実装。

--- data/text-translate.js
+++ data/text-translate.js
@@ -2,9 +2,17 @@
 $(function() {
   $("#translate-button").click(function(){
     var msg = {
+      kind: 'translate',
       from: $('#from-lang-select option:selected').val(),
       to: $('#to-lang-select option:selected').val(),
       text:$("#src > textarea").val()
+    };
+    self.postMessage(msg);
+  });
+
+  $("#close-button").click(function() {
+    var msg = {
+      kind: 'close'
     };
     self.postMessage(msg);
   });
