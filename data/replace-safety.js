0dd04f5d111419e67f8d456b22d2e3d06d6d7d24
ongaeshi <ongaeshi0621@gmail.com>
2011-11-29 01:58:37 +0900

    escapeHTML(), "'" -> "&apos;" への変換を追加

--- data/replace-safety.js
+++ data/replace-safety.js
@@ -3,7 +3,7 @@
 // @author ongaeshi
 // @date   2011/11/23
 
-function escapeHTML(str) str.replace(/[&"<>]/g, function (m) "&" + ({ "&": "amp", '"': "quot", "<": "lt", ">": "gt" })[m] + ";");
+function escapeHTML(str) str.replace(/[&"'<>]/g, function (m) "&" + ({ "&": "amp", '"': "quot", "'": "apos", "<": "lt", ">": "gt" })[m] + ";");
 
 var replaceTextSafety = function(node, text) {
   // node.replaceWholeText(text);
