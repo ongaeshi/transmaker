30b8d9ef4b9709a82a77c10339675302a3332dbe
ongaeshi <ongaeshi0621@gmail.com>
2011-12-01 00:43:05 +0900

    node.textContent に代入する時にescapeHTMLしていたので、二重にescapeがかかっていた問題を修正。

--- data/replace-safety.js
+++ data/replace-safety.js
@@ -3,11 +3,9 @@
 // @author ongaeshi
 // @date   2011/11/23
 
-function escapeHTML(str) str.replace(/[&"'<>]/g, function (m) "&" + ({ "&": "amp", '"': "quot", "'": "apos", "<": "lt", ">": "gt" })[m] + ";");
-
 var replaceTextSafety = function(node, text) {
   // node.replaceWholeText(text);
-  node.textContent = escapeHTML(text);
+  node.textContent = text;
 }
 
 
